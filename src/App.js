1c4a852abf0767a2a1a5abc73fd3efee62c3ccf5
Sara Torrey <sara.moravej@gmail.com>
2019-04-25 15:50:14 -0500

    Add layout & book-list folders Add book files & Header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    pages: []
+  }
+
+render () {
+    return (
+        <Router>
+          <div className="App">
+            <div className="container">
+              <Header />
+                <Route exact path="/" render={props => (
+                    <React.Fragment>
+                      <LessonBook />
+                      <TheoryBook />
+                      <TechniqueBook />
+                      <PerformanceBook />
+                    </React.Fragment>
+                )} />
+            </div>
+          </div>
+        </Router>
+    )
+}
+
 }
 
 export default App;
---
+++ src/components/layout/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header () {
+    return(
+        <header style={headerStyle}>
+            <h1>Practice Sheet</h1>
+        </header>
+    )
+}
+
+export default Header;
